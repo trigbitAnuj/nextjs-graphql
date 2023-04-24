7753b7986d211c9dd219099aa64496603b2d5372
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-24 13:57:44 +0530

    Create ClientsOnly.tsx

---
+++ components/ClientsOnly.tsx
@@ -0,0 +1,20 @@
+import   { useEffect, useState } from "react";
+
+interface Props{
+    children:React.ReactNode
+}
+
+ const  ClientOnly=({ children }:Props)=> {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div >{children}</div>;
+}
+export default ClientOnly
